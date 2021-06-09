2ee6af266d5266070abab8d14b57904294ba48b1
Ronaldo Macapobre <RonaldoM@magenic.com>
2021-06-09 20:06:29 +0800

    Exam 1 changes

---
+++ main.js
@@ -0,0 +1,64 @@
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
+
+class Bank {
+  constructor(loanAmount, interestRate) {
+    this.loanAmount = loanAmount;
+    this.interestRate = interestRate;
+  }
+
+  getMonthlyInstallment(loanTerm) {
+    return (
+      (this.loanAmount * this.interestRate * loanTerm + this.loanAmount) /
+      loanTerm
+    );
+  }
+}
+
+class Metrobank extends Bank {
+  constructor(loanAmount) {
+    super(loanAmount, 0.015);
+  }
+}
+
+class BPI extends Bank {
+  constructor(loanAmount) {
+    super(loanAmount, 0.012);
+  }
+}
+
+class BDO extends Bank {
+  constructor(loanAmount) {
+    super(loanAmount, 0.017);
+  }
+}
+
+class LoanCalculator {
+  constructor(bankName, loanAmount, loanTerm) {
+    this.bankName = bankName;
+    this.loanAmount = loanAmount;
+    this.loanTerm = loanTerm;
+  }
+
+  getBank() {
+    switch (this.bankName) {
+      case 'metrobank':
+        return new Metrobank(this.loanAmount);
+      case 'bpi':
+        return new BPI(this.loanAmount);
+      case 'bdo':
+        return new BDO(this.loanAmount);
+    }
+  }
+
+  getMonthlyInstallment() {
+    const bank = this.getBank();
+    return bank.getMonthlyInstallment(this.loanTerm);
+  }
+}
+
+const { bankName, loanAmount, loanTerm } = argv;
+const loanCalc = new LoanCalculator(bankName, loanAmount, loanTerm);
+
+console.log(`Monthly Installment: ${loanCalc.getMonthlyInstallment()}`);
