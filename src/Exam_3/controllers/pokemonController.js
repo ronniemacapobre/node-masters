241f0676a1de1fbe7244cc723e2208bcf6085780
Ronaldo Macapobre <RonaldoM@magenic.com>
2021-06-26 00:39:46 +0800

    Implement getByName, update and delete

--- src/Exam_3/controllers/pokemonController.js
+++ src/Exam_3/controllers/pokemonController.js
@@ -1,43 +1,98 @@
 const { pokemonService } = require('../services');
 const url = require('url');
 
+const writeSuccessResponse = (res, result) => {
+  res.writeHead(200, {
+    'Content-Type': 'application/json',
+  });
+  res.write(JSON.stringify(result));
+  res.end();
+};
+
+const writeFailedResponse = (res, result) => {
+  res.writeHead(400, {
+    'Content-Type': 'application/json',
+  });
+  res.write(JSON.stringify(result));
+  res.end();
+};
+
 exports.handleGetRequest = (req, res) => {
-    const data = pokemonService.get();
+  const { searchParams } = new URL(req.url, `http://${req.headers.host}`);
 
-    const result = { data };
+  const data = searchParams.has('name')
+    ? pokemonService.getByName(searchParams.get('name'))
+    : pokemonService.get();
 
-    res.writeHead(200, {
-        'Content-Type': 'application/json',
-    });
-    res.write(JSON.stringify(result));
-    res.end();
+  const result = { data };
+
+  writeSuccessResponse(res, result);
 };
 
 exports.handlePostRequest = (req, res) => {
-    const data = [];
+  const data = [];
 
-    req.on('data', (chunk) => {
-        data.push(chunk);
+  req.on('data', (chunk) => {
+    data.push(chunk);
+  });
+
+  req.on('end', () => {
+    const parsedData = Buffer.concat(data).toString();
+    const dataJson = JSON.parse(parsedData);
+
+    const result = pokemonService.insert(dataJson);
+
+    if (!result.success) {
+      res.writeHead(400, {
+        'Content-Type': 'application/json',
+      });
+      res.write(JSON.stringify(result));
+      res.end();
+    }
+
+    writeSuccessResponse(res, result);
+  });
+};
+
+exports.handlePutRequest = (req, res) => {
+  const { searchParams } = new URL(req.url, `http://${req.headers.host}`);
+
+  if (!searchParams.has('name')) {
+    writeFailedResponse(res, {
+      success: false,
+      errorMessage: 'Name parameter not found.',
     });
+  }
 
-    req.on('end', () => {
-        const parsedData = Buffer.concat(data).toString();
-        const dataJson = JSON.parse(parsedData);
+  const data = [];
 
-        const result = pokemonService.insert(dataJson);
+  req.on('data', (chunk) => {
+    data.push(chunk);
+  });
 
-        if (!result.success) {
-            res.writeHead(400, {
-                'Content-Type': 'application/json',
-            });
-            res.write(JSON.stringify(result));
-            res.end();
-        }
+  req.on('end', () => {
+    const parsedData = Buffer.concat(data).toString();
+    const dataJson = JSON.parse(parsedData);
 
-        res.writeHead(200, {
-            'Content-Type': 'application/json',
-        });
-        res.write(JSON.stringify(result));
-        res.end();
+    const result = pokemonService.update(searchParams.get('name'), dataJson);
+
+    if (!result.success) writeFailedResponse(res, result);
+    else writeSuccessResponse(res, result);
+  });
+};
+
+exports.handleDeleteRequest = (req, res) => {
+  const { searchParams } = new URL(req.url, `http://${req.headers.host}`);
+
+  if (!searchParams.has('name')) {
+    writeFailedResponse(res, {
+      success: false,
+      errorMessage: 'Name parameter not found.',
     });
+  }
+
+  const name = searchParams.get('name');
+  const result = pokemonService.delete(name);
+  if (!result.success) writeFailedResponse(res, result);
+  else writeSuccessResponse(res, result);
 };
--- src/Exam_3/services/pokemonService.js
+++ src/Exam_3/services/pokemonService.js
@@ -11,6 +11,23 @@ exports.get = () => {
   return pokemons;
 };
 
+exports.getByName = (name) => {
+  const pokemon = db
+    .get('pokemons')
+    .find((val) => {
+      return val.name.toLowerCase() === name.toLowerCase();
+    })
+    .value();
+
+  if (!pokemon)
+    return {
+      success: false,
+      errorMessage: `Pokemon ${name.toUpperCase()} not exist.`,
+    };
+
+  return pokemon;
+};
+
 exports.insert = (pokemon) => {
   const { name } = pokemon;
 
@@ -23,11 +40,66 @@ exports.insert = (pokemon) => {
   if (isPokemonExist) {
     return {
       success: false,
-      errorMessage: `Pokemon ${name} already exist.`,
+      errorMessage: `Pokemon ${name.toUpperCase()} already exist.`,
     };
   }
 
   db.get('pokemons').push(pokemon).write();
+
+  return {
+    success: true,
+  };
+};
+
+exports.update = (name, pokemon) => {
+  const isPokemonExist =
+    db
+      .get('pokemons')
+      .value()
+      .filter((_) => _.name === name).length > 0;
+
+  if (!isPokemonExist)
+    return {
+      success: false,
+      errorMessage: `Pokemon ${name.toUpperCase()} not exist.`,
+    };
+
+  const { type, generation } = pokemon;
+
+  if (!type)
+    return {
+      success: false,
+      errorMessage: `Missing type`,
+    };
+
+  db.get('pokemons')
+    .find({ name })
+    .assign({
+      type,
+      generation,
+    })
+    .write();
+
+  return {
+    success: true,
+  };
+};
+
+exports.delete = (name) => {
+  const isPokemonExist =
+    db
+      .get('pokemons')
+      .value()
+      .filter((_) => _.name === name).length > 0;
+
+  if (!isPokemonExist) {
+    return {
+      success: false,
+      errorMessage: `Pokemon ${name.toUpperCase()} does not exist.`,
+    };
+  }
+
+  db.get('pokemons').remove({ name }).write();
 
   return {
     success: true,
